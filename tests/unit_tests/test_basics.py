854b640e63ca97b26bc195eadf95e17436f99343
joanibal <joanibal@umich.edu>
2020-02-05 15:03:23 -0500

    test

---
+++ tests/unit_tests/test_basics.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import unittest
+import numpy
+
+import os
+import sys
+baseDir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(baseDir,'../../'))
+from python.pyADflow import ADFLOW
+
+
+class BasicTests(unittest.TestCase):
+    N_PROCS = 1
+
+    def test_import(self):
+        gridFile = 'input_files/mdo_tutorial_euler.cgns'
+        options = {'gridfile': gridFile}
+        CFDSolver = ADFLOW(options=options, debug=False)
